331fa0892d68233253525e11da7f1424f599f52e
Natasha Ho <natashaho1@gmail.com>
2019-05-07 20:27:02 -0400

    Todo App Phase 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+           <input type="checkbox" />item 1<br />
+           <input type="checkbox" />item 2<br />
+           <input type="checkbox" />item 3<br />
+        </div>
+      );
+  }
+}
+
+export default App;
