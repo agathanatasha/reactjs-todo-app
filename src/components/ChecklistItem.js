e1b5cdc013f33f00ca773bb8224d11e214b986a1
Natasha Ho <natashaho1@gmail.com>
2019-05-09 12:13:39 -0400

    grey out completed items

--- src/components/ChecklistItem.js
+++ src/components/ChecklistItem.js
@@ -4,7 +4,8 @@ import React from 'react';
 function ChecklistItem(props){
     return(
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed} onChange={()=> props.handleChange(props.item.id)}/>{props.item.text}<br/>
+            <input id="todo-checkbox" type="checkbox" checked={props.item.completed} onChange={()=> props.handleChange(props.item.id)}/>
+            <label htmlFor="todo-checkbox">{props.item.text}</label><br/>
         </div>
         
     );
